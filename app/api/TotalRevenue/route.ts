7ac409bc8fcc7eafdfccfd84d48fc9821c352635
prithvikr66 <pkunofficial66@gmail.com>
2024-05-23 17:40:27 +0530

    fixed month filter

--- app/api/TotalRevenue/route.ts
+++ app/api/TotalRevenue/route.ts
@@ -5,6 +5,7 @@ export async function GET(req: NextRequest) {
   const url = new URL(req.url);
   const interval = url.searchParams.get("interval");
   try {
+    console.log(interval);
     const response = await axios.get(
       `https://staging-reportingapi.cryptocart.cc/orders/revenue?reportDate=2024-01-20&interval=${interval}`
     );
@@ -53,6 +54,48 @@ export async function GET(req: NextRequest) {
       const weekOrder = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
       const itemsInOrder = weekOrder.map((week) =>
         items.find((item: any) => item.segmentName === week)
+      );
+
+      const labels = itemsInOrder.map((item) => item.segmentName);
+      const datas = itemsInOrder.map((item) => item.segmentTotal);
+      return NextResponse.json({
+        labels,
+        datas,
+        totalOrders,
+      });
+    } else if (interval === "Month") {
+      const daysOrder = [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20",
+        "24",
+        "25",
+        "26",
+        "27",
+        "28",
+        "29",
+        "30",
+        "31",
+      ];
+      const itemsInOrder = daysOrder.map((day) =>
+        items.find((item: any) => item.segmentName === day)
       );
 
       const labels = itemsInOrder.map((item) => item.segmentName);
